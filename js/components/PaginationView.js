74fe08348019628d227386d7303dea0933b7cc4b
alexisnisol <alexis.nisol@etu.univ-orleans.fr>
2025-03-26 13:36:39 +0100

    Fix rechargement pagination

--- js/components/PaginationView.js
+++ js/components/PaginationView.js
@@ -11,7 +11,7 @@ export default class PaginationView {
         if (currentPage > 1) {
             paginationHTML += /*html*/`
                 <li class="page-item">
-                    <a class="page-link" href="#" data-page="${currentPage - 1}">Précédent</a>
+                    <a class="page-link" data-page="${currentPage - 1}">Précédent</a>
                 </li>
             `;
         }
@@ -19,7 +19,7 @@ export default class PaginationView {
         for (let i = 1; i <= totalPages; i++) {
             paginationHTML += /*html*/`
                 <li class="page-item ${i === currentPage ? 'active' : ''}">
-                    <a class="page-link" href="#" data-page="${i}">${i}</a>
+                    <a class="page-link" data-page="${i}">${i}</a>
                 </li>
             `;
         }
@@ -27,7 +27,7 @@ export default class PaginationView {
         if (currentPage < totalPages) {
             paginationHTML += /*html*/`
                 <li class="page-item">
-                    <a class="page-link" href="#" data-page="${currentPage + 1}">Suivant</a>
+                    <a class="page-link" data-page="${currentPage + 1}">Suivant</a>
                 </li>
             `;
         }
@@ -38,9 +38,12 @@ export default class PaginationView {
         const paginationLinks = document.querySelectorAll('.page-link');
         paginationLinks.forEach(link => {
             link.addEventListener('click', async (event) => {
-                event.preventDefault();
                 const page = parseInt(link.dataset.page);
                 await this.paginationHandler.changePage(page);
+                // On remplace le contenu de la page par le contenu de la vue
+                const content = null || document.querySelector('#content');
+                content.innerHTML = await this.view.render();
+                await this.view.after_render();
             });
         });
     }
