0bab237956f1e4e5571d0aa51c6c2617a2dbb204
AlexyWCK <alex.wiciak@gmail.com>
2025-03-28 12:32:00 +0100

    ajout bouton retour

--- js/views/pages/operators/OperatorDetail.js
+++ js/views/pages/operators/OperatorDetail.js
@@ -29,7 +29,8 @@ export default class OperatorDetail extends Views {
             <section class="operator-detail">
                 <div class="main-background">
                     <div class="background-overlay"></div>
-                    
+                    <button class="back-button" onclick="window.history.back()">&#8592; Retour</button>
+
                     <img src="/static/img/operators/${operator.image}" alt="${operator.nom}" class="operator-character">
                     
                     <div class="detail-content">
